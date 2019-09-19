e0f9b03ba86d00849f70d3d4524b1977dca77351
bihire <muhireboris@yahoo.fr>
2019-09-19 19:02:46 +0200

    feat(server): Employee can sign up - Employee can create an account - Unique email for every Employee - receive a jwt token as a successful response - recieve a error message as failer message [Finishes #168387852]

--- api/v1/index.js
+++ api/v1/index.js
@@ -1,11 +1,11 @@
 // const authRoutes = require('./routes/auth');
 // const propertyRoutes = require('./routes/property');
 // const flagRoutes = require('./routes/flag');
-const articleRoutes = require('./routes/article')
+import authRoutes from "./routes/auth"
 
-module.exports = (app) => {
-    // app.use('/api/v1', authRoutes);
-    // app.use('/api/v1', propertyRoutes);
-    // app.use('/api/v1', flagRoutes);
-    app.use('/api/v1', articleRoutes);
+module.exports = app => {
+  // app.use('/api/v1', authRoutes);
+  // app.use('/api/v1', propertyRoutes);
+  // app.use('/api/v1', flagRoutes);
+  app.use("/api/v1", authRoutes);
 };
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-module.exports = (app) => {
-    app.get('/', (req, res) => {
-        res.send('bro you are in the index')
-    })
-}
+module.exports = app => {
+  app.get("/", (req, res) => {
+    res.send("bro you are in the index");
+  });
+};
