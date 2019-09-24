eb2a14acfcbb99b849bbb5311dfe7980bcb7b5d4
bihire <muhireboris@yahoo.fr>
2019-09-25 01:20:57 +0200

    feat(server): Employee can update their own articles - Employee can update their own articles - keep unchenged fields unchanged - validation of new data - response format to the Employee [Finishes #168388113]

--- api/v1/routes/article.js
+++ api/v1/routes/article.js
@@ -2,9 +2,11 @@ import express from 'express'
 import articleController from '../controllers/articleController'
 import jwt from "../middlewares/jwt"
 import createArticle from "../middlewares/articleValidation/createArticle"
+import updateArticle from "../middlewares/articleValidation/updateArticle"
 
 const router = express.Router();
 router.post("/articles", jwt, createArticle, articleController.create);
+router.patch("/articles/:articleId", jwt, updateArticle, articleController.update);
 // router.post("/auth/signin", signin, authanticationController.login);
 
 export default router;
