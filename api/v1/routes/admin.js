461eca116fbbc4dcf0cded36d28cb54fc62d9326
bihire <muhireboris@yahoo.fr>
2019-09-28 19:25:02 +0200

    feat(server): Admin can delete flagged items - Admin can delete a flagged articles - Admin can delete a flagged comment - validations before delete - delete everything associated [Finishes #168388570] [Finishes #168388529]

--- api/v1/routes/admin.js
+++ api/v1/routes/admin.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import adminController from '../controllers/adminController'
 import adminIgnoreController from '../controllers/adminIgnoreController'
+import adminDeleteController from '../controllers/adminDeleteController'
 import admin from "../middlewares/adminValidation/admin"
 
 import jwt from "../middlewares/jwt"
@@ -13,5 +14,10 @@ router.get("/admin/comments", jwt, admin, adminController.fetch_comment)
 
 router.delete("/admin/:flagId/ignore/articles", jwt, admin, adminIgnoreController.article)
 router.delete("/admin/:flagId/ignore/comments", jwt, admin, adminIgnoreController.comment)
+
+// --------------- Admin can delete flagged items ----------------
+
+router.delete("/admin/:flagId/delete/articles", jwt, admin, adminDeleteController.article)
+router.delete("/admin/:flagId/delete/comments", jwt, admin, adminDeleteController.comment)
 
 export default router
