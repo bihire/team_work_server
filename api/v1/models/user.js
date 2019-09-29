b7dd6caccc3c43a0c3dc2767b095087aac0042a8
bihire <muhireboris@yahoo.fr>
2019-09-29 21:15:39 +0200

    feat(server): tests of endpoints - tests of 18points [Finishes #168826754]

--- api/v1/models/user.js
+++ api/v1/models/user.js
@@ -1,2 +1,13 @@
-const users = [];
+const users = [{
+    "id": 1,
+    "firstName": "bihire",
+    "lastName": "boris",
+    "email": "muhire@yahoo.fr",
+    "address": "bro",
+    "password": "$2a$08$lHQ5bvGMqB/DXMUW0rf/OeULgc2bszkc4KAVLqJGSncS7iTknypFO",
+    "gender": "other",
+    "jobRole": "assisstent",
+    "department": "electrical",
+    "isAdmin": true
+}];
 module.exports = users;
