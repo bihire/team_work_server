f8eb53c50334863ff280c01e80f4a289be07de72
bihire <muhireboris@yahoo.fr>
2019-10-08 18:39:11 +0200

    ch(server): add async on hashpassword function - add sync on hash function [Finishes #169017179]

--- api/v1/heplpers/hash.js
+++ api/v1/heplpers/hash.js
@@ -1,6 +1,6 @@
 import bcryptjs from 'bcryptjs'
 
-const hashPassword = password => {
+const hashPassword = async password => {
         const salt = await bcryptjs.genSalt(10);
         const hashed = await bcryptjs.hash(password, salt);
         return hashed;
