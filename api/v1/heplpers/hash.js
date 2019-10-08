82d5e91aff2053a3352b42a7793925ba5241520a
bihire <muhireboris@yahoo.fr>
2019-10-08 18:33:49 +0200

    ch(server): change hashing function to async - change hashing password function from promise to async wait long enough until it resolves [Finishes #169017179]

--- api/v1/heplpers/hash.js
+++ api/v1/heplpers/hash.js
@@ -2,13 +2,9 @@ import bcryptjs from 'bcryptjs'
 const saltRounds = 8
 
 const hashPassword = password => {
-
-    const hashedPassword = new Promise((resolve, reject) => {
-        bcryptjs.hash(password, saltRounds, function (err, hash) {
-            if (err) reject(err)
-            resolve(hash)
-        });
-    })
-    return hashedPassword
+        const salt = await bcrypt.genSalt(10);
+        const hashed = await bcrypt.hash(password, salt);
+        return hashed;
+    
 }
 export default hashPassword
