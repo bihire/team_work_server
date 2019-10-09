253db003838916d6bb40d7c153538ec8eaeb6a32
bihire <muhireboris@yahoo.fr>
2019-10-09 10:11:31 +0200

    ft(server): Employee can signup - create a package.json script to create a userTable if it doesn't exist - create a user schema in myDb directory - return error if user email exist already - save the new user in the database if success [Delivers #169014320]

--- config/myDb.js
+++ config/myDb.js
@@ -8,13 +8,41 @@ const pool = new Pool({
     password: process.env.PASSWORD,
     post: process.env.POST,
 })
-pool.connect((err,res,done)=> {
-    if(err) {
-        console.log(err)
-    } else {
-    console.log('connected to the Database');        
-    }
-})
+
+pool.on('connect', () => {
+    console.log('connected to the Database');
+});
+
+const createTables = () => {
+    const userTable = `CREATE TABLE IF NOT EXISTS
+      users(
+        id SERIAL PRIMARY KEY,
+        first_name VARCHAR(25) NOT NULL,
+        email VARCHAR(25) UNIQUE NOT NULL,
+        last_name VARCHAR(25) NOT NULL,
+        address VARCHAR(128) NOT NULL,
+        department VARCHAR(25) NOT NULL,
+        is_admin BOOLEAN NOT NULL,
+        gender VARCHAR(25) NOT NULL,
+        job_role VARCHAR(128) NOT NULL,
+        password VARCHAR(128) NOT NULL
+      )`;
+    pool.query(userTable)
+        .then((res) => {
+            console.log(res);
+            pool.end();
+        })
+        .catch((err) => {
+            console.log(err);
+            pool.end();
+        });
+};
+
+pool.on('remove', () => {
+    console.log('client removed');
+    process.exit(0);
+});
 
 
-export default pool
+module.exports = { createTables, pool };
+require('make-runnable')
